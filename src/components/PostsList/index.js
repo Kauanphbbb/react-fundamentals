440f6d1a183e40494146c828b45c4bafba1349e6
Kauanphbbb <kauanphbbb@gmail.com>
2024-01-08 08:16:21 -0300

    class components - render method

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
-import { Container } from './styles';
 import Post from './Post';
+import { Container } from './styles';
 
 import posts from './posts';
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+export default class PostsList extends React.Component {
+  render() {
+    return (
+      <Container>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </Container>
+    );
+  }
 }
+
+// export default function PostsList() {
+//   return (
+//     <Container>
+//       {posts.map((post) => (
+//         <Post
+//           key={post.id}
+//           title={post.title}
+//           description={post.description}
+//         />
+//       ))}
+//     </Container>
+//   );
+// }
